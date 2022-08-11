7d66db1eaee68ccde79f7f52583f000ca44eac20
kurma5147 <84989931+kurma5147@users.noreply.github.com>
2022-08-11 17:21:43 +0530

    Update ecs-auto.sh

--- ecs-auto.sh
+++ ecs-auto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 SERVICE="sampledeploy"
 CLUSTER_NAME="sampledeploy"
-AWS_REGION="ap-south-1"
+AWS_REGION="us-east-1"
 export AWS_PROFILE=default
 
 # Register a new Task definition 
-aws ecs register-task-definition --family anuj-new --cli-input-json file://task-new.json --region $AWS_REGION
+aws ecs register-task-definition --family sampledeploy --cli-input-json file://task-new.json --region $AWS_REGION
 
 # Update Service in the Cluster
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition anuj-new --desired-count 1 --region $AWS_REGION
+aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition sampledeploy --desired-count 1 --region $AWS_REGION
 
