67cf9e83a6c7eda7119c02dd79824c12163bf771
kurma5147 <84989931+kurma5147@users.noreply.github.com>
2022-08-12 15:01:16 +0530

    Update ecs-auto.sh

--- ecs-auto.sh
+++ ecs-auto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-SERVICE="sampledeploy"
-CLUSTER_NAME="sampledeploy"
+SERVICE="siva"
+CLUSTER_NAME="siva"
 AWS_REGION="us-east-1"
 export AWS_PROFILE=default
 
 # Register a new Task definition 
-aws ecs register-task-definition --family sampledeploy --cli-input-json file://task-new.json --region $AWS_REGION
+aws ecs register-task-definition --family siva --cli-input-json file://task-new.json --region $AWS_REGION
 
 # Update Service in the Cluster
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition sampledeploy --desired-count 1 --region $AWS_REGION
+aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition siva --desired-count 1 --region $AWS_REGION
 
